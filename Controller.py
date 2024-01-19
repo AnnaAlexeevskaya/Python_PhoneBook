388916afd7faa8db28273eaf9025e6d2cad89c51
Anna Alekseevskaya <Anna.Alekseevskaya@waveaccess.ru>
2024-01-19 23:16:36 +0400

    test2

--- Controller.py
+++ Controller.py
@@ -10,6 +10,7 @@ def start_app():
         match choice:
             case 1:
                 pb = Model.open_file()
+                print(pb)
             case 2:
                 pass            
             case 3:
