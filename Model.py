40ac9f41da63ae9da804b4cf60143df6886e960d
Anna Alekseevskaya <Anna.Alekseevskaya@waveaccess.ru>
2024-01-19 23:26:40 +0400

    test2

--- Model.py
+++ Model.py
@@ -6,7 +6,7 @@ SEPARATOR = ';'
 def open_file():
     global phone_book
     with open(path, 'r', encoding='UTF-8') as file:
-        phone_book = {i: item for i, item in enumerate(list(map(lambda x: x.strip().split('SEPARATOR'), file.readlines())), 1)}
+        phone_book = {i: item for i, item in enumerate(list(map(lambda x: x.strip().split(SEPARATOR), file.readlines())), 1)}
     print(phone_book)   
     return phone_book
 
