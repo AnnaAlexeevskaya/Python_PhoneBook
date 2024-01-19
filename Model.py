edccacd61cfd82bb864bea18d0938cc295b9fc97
Anna Alekseevskaya <Anna.Alekseevskaya@waveaccess.ru>
2024-01-19 23:20:13 +0400

    test2

--- Model.py
+++ Model.py
@@ -8,5 +8,6 @@ def open_file():
     with open(path, 'r', encoding='UTF-8') as file:
         phone_book = {i: item for i, item in 
                       enumerate(list(map(lambda x: x.strip().split('SEPARATOR'), file.readlines())), 1)}
+        print(phone_book)
     return phone_book
 
