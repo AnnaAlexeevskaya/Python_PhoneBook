8ecdee870cc9fa6a1d4a462c612e6d6ed37d96c8
Anna Alekseevskaya <Anna.Alekseevskaya@waveaccess.ru>
2024-01-19 19:05:35 +0400

    test commit

---
+++ Controller.py
@@ -0,0 +1,25 @@
+import text
+import View
+import Model
+
+
+def start_app():
+    while True:
+        choice = View.main_menu()
+        match choice:
+            case 1:
+                model.open_file()
+            case 2:
+                pass            
+            case 3:
+                pass            
+            case 4:
+                pass            
+            case 5:
+                pass            
+            case 6:
+                pass            
+            case 7:
+                pass
+            case 8:
+                break
