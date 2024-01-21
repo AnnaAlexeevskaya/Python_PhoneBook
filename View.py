9bea8e24681485a1047f3f1e3d6189bc560348d7
Anna Alekseevskaya <Anna.Alekseevskaya@waveaccess.ru>
2024-01-21 13:25:40 +0400

    test2

--- View.py
+++ View.py
@@ -33,18 +33,13 @@ def print_message(message: str):
     print('='*len(message) + '\n')
 
 
-def add_contact(message: list[str], contact: Contact = None): #list[str] = None
+def add_contact(message: list[str], contact: list[str] = None): #list[str] = None
     # (message: list[str], contact: Contact): #list[str] = None
-    #contact = contact if contact else ['', '', '']
+    contact = contact if contact else ['', '', '']
     for n, mes in enumerate(message):
         field = input(mes)
-        # contact[n] = field if field else contact[n]
+        contact[n] = field if field else contact[n]
         #contact.name = field if field else  contact.name
-        contact.name = field if field else  contact.name
-        field = input(mes)
-        contact.phone = field if field else  contact.phone
-        field = input(mes)
-        contact.comment = field if field else  contact.comment
     return contact
 
 
