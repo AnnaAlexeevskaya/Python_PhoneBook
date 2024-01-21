fa79b0745d8cdfe84d216c355df99f1f4fae5df6
Anna Alekseevskaya <Anna.Alekseevskaya@waveaccess.ru>
2024-01-21 10:38:09 +0400

    test2

--- Model.py
+++ Model.py
@@ -53,7 +53,7 @@ class PhoneBook:
     #contact.name:<{max_size[0]}} {contact.phone:<{max_size[1]}} {contact.comment:<{max_size[2]}
 #contact.name[0], contact.phone[1], contact.comment[2]
     def change_contact(self, c_id: int, c_contact: list[str]):
-        self.phonebook[c_id] = Contact(*c_contact)
+        self.phonebook[c_id] = c_contact
 
     def delete_contact(self, c_id: int) -> list[str]:
         return self.phonebook.pop(c_id)
